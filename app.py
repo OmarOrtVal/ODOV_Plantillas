39d363d3fce0f8b38ad60b275788e9ecd5d9fed7
OmarOrtVal <23308060610674@cetis61.edu.mx>
2025-10-06 09:08:57 -0600

    trabajo de la clase

--- app.py
+++ app.py
@@ -4,7 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    arr = ["Padilla","Renteria","Soto"]
+    autor = "Omar Daniel Ortega Valtierra"
+    return render_template("plantilla.html", nombre = autor, amigos = arr)
+
+@app.route("/ciudades")
+def ciudades():
+    arr = ["Italia","Bangkok","New York"]
+    autor = "Omar Daniel Ortega Valtierra"
+    return render_template("plantilla1.html", nombre = autor, ciudades = arr)
+
+@app.route("/colores")
+def colores():
+    arr = ["Azul","Rosa","Rojo"]
+    autor = "Omar Daniel Ortega Valtierra"
+    return render_template("plantilla2.html", nombre = autor, colores = arr)
 
 if __name__ == "__main__":
     app.run(debug=True)
